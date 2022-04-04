976f69be6d69a3fd907263fc4f191e51af5d2469
Jeremy Chase <jeremy.chase@gmail.com>
2022-04-04 15:54:43 -0400

    add memfd_secret systemcall

--- src/cmd/vendor/golang.org/x/sys/unix/syscall_linux.go
+++ src/cmd/vendor/golang.org/x/sys/unix/syscall_linux.go
@@ -1855,6 +1855,7 @@ func Getpgrp() (pid int) {
 //sys	Lremovexattr(path string, attr string) (err error)
 //sys	Lsetxattr(path string, attr string, data []byte, flags int) (err error)
 //sys	MemfdCreate(name string, flags int) (fd int, err error)
+//sys	MemfdSecret(flags int) (fd int, err error)
 //sys	Mkdirat(dirfd int, path string, mode uint32) (err error)
 //sys	Mknodat(dirfd int, path string, mode uint32, dev int) (err error)
 //sys	Nanosleep(time *Timespec, leftover *Timespec) (err error)
